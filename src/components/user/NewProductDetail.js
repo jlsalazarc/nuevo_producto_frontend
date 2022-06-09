0d81555cbea87b97f9945aa9a0be75eeeffbaaf2
Jorge <jorge.salazar.cavero@gmail.com>
2022-06-08 20:45:06 -0500

    adding nuevos productos button

---
+++ src/components/user/NewProductDetail.js
@@ -0,0 +1,48 @@
+import './NewProductDetail.css'
+import { useState,useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import { Header } from '../../components/user/header'; 
+
+
+export function DestinyDetail () {
+    const navigate =useNavigate ();
+    const {destinyId} = useParams();
+    const [destinydetail, setDestinydetail] = useState ([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+   
+
+    useEffect(() =>{
+        axios({
+            method:'GET',
+            baseURL: process.env.BASE_URL,
+            url:`/proyectos/${destinyId}`
+        })
+            .then(({data}) => setDestinydetail(data))
+            .catch(() => setError(true))
+            .finally(() => setLoading(false))
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>error</p>
+    },[]);
+
+
+    return(
+        <>
+        <div className = "index" >
+            <header className='header'>
+                <Header/>
+            </header>
+            <div className = "body_destinydetail">
+                <div className = "body_destinydetail_description">            {console.log(destinydetail.Descripcion)}
+                    <p>{destinydetail.Descripcion}</p>
+                </div>
+                <div className = "body_destinydetail_description">
+                    <h2>Comentarios</h2>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/components/user/body.js
+++ src/components/user/body.js
@@ -1,9 +1,14 @@
 import './body.css'
+import np from '../img/nuevos_productos.png'
 import crea from '../img/crear.png'
 import modifica from '../img/Modificar.png'
 export function Body(){
     return (
 <div className="destinosCard-container">
+    <div className="destinosCard-container_img">
+        <img src={np} alt="crear" width="240" height="192"/>
+        <h3>Mis Nuevos Productos</h3>
+    </div>
     <div className="destinosCard-container_img">
         <img src={crea} alt="crear" width="240" height="192"/>
         <h3>Crear Nuevo Producto</h3>
